416d4671f35cdd7530501a8a972d53df5ed98544
Daniel Gonzalez <daniel.gonzalez13@uabc.edu.mx>
2025-11-21 21:40:25 -0800

    Example connection to AWS

--- main.py
+++ main.py
@@ -2,66 +2,56 @@ import time
 import machine
 import network
 import ssl
-import ntptime # <--- Necesario para la hora
+import ntptime
+import secrets
 from umqtt.simple import MQTTClient
 
-# Nombres de los archivos que subiste
-KEY_PATH = "/private.pem.key"
-CERT_PATH = "/cert.pem.crt"
-# Nota: MicroPython a veces no necesita el Root CA explícitamente si no verificas el servidor rigurosamente, 
-# pero es buena práctica tenerlo. Para simplificar este ejemplo, usaremos los dos principales.
+KEY_PATH = "certs/private.pem.key"
+CERT_PATH = "certs/cert.pem.crt"
 
-# --- CONEXIÓN WIFI ---
 def connect_wifi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        print('Conectando a WiFi...')
-        wlan.connect(WIFI_SSID, WIFI_PASS)
+        print('Connecting to WiFi...')
+        wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASS)
         while not wlan.isconnected():
             pass
-    print('¡WiFi Conectado! IP:', wlan.ifconfig()[0])
+    print('WiFi Connected! IP:', wlan.ifconfig()[0])
 
 def set_time():
-    print("Sincronizando hora con NTP...")
     try:
         ntptime.settime()
         # Imprimir la fecha actual para verificar (Año/Mes/Dia ...)
-        print("Hora actual:", time.localtime()) 
+        print("Actual time:", time.localtime()) 
     except Exception as e:
-        print("Error sincronizando hora, reintentando...", e)
+        print("Error while synchronizing hour", e)
         
-# --- LEER CERTIFICADOS ---
 def read_file(filename):
     with open(filename, 'rb') as f:
         return f.read()
 
-# --- PROGRAMA PRINCIPAL ---
 def run():
     connect_wifi()
     set_time()
-    # Leer claves
-    print("Leyendo certificados...")
+
+    # Reading keys
     key_data = read_file(KEY_PATH)
-    cert_data = read_file(CERT_PATH)
+    cert_data = read_file(CERT_PATH)    
     
-    # --- AGREGA ESTO ---
-    print("Tamaño Clave:", len(key_data))
-    print("Tamaño Certificado:", len(cert_data))
-    
-    # Configurar SSL
+    # SSL
     ssl_params = {
         "key": key_data,
         "cert": cert_data,
         "server_side": False,
-        "server_hostname": AWS_ENDPOINT
+        "server_hostname": secrets.AWS_ENDPOINT
     }
     
-    # Conectar a AWS MQTT
-    print("Conectando a AWS...")
+    # Connect to AWS MQTT
+    print("Connecting to AWS...")
     client = MQTTClient(
-        client_id=CLIENT_ID,
-        server=AWS_ENDPOINT,
+        client_id=secrets.CLIENT_ID,
+        server=secrets.AWS_ENDPOINT,
         port=8883,
         keepalive=1200,
         ssl=True,
@@ -70,19 +60,18 @@ def run():
     
     try:
         client.connect()
-        print("¡Conectado a AWS IoT!")
+        print("Connected to AWS IoT!")
         
         while True:
             msg = '{"temperatura": 23.5, "estado": "funcionando"}'
-            client.publish(TOPIC, msg)
+            client.publish(secrets.TOPIC, msg)
             print("Enviado:", msg)
             time.sleep(10)
             
     except Exception as e:
         print("Error:", e)
-        # Reiniciar si falla
         time.sleep(10)
         machine.reset()
 
-# Ejecutar
+# Execute
 run()
